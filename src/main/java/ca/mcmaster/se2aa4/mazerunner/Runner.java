93aa6f8b2a06e74c464f73341538395d70efebde
Mariaakhtar0035 <mariaakhtar0035@gmail.com>
2025-01-28 11:40:17 -0500

    feat: #10 introduce path validation for factorized form

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Runner.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Runner.java
@@ -5,16 +5,28 @@ import org.apache.logging.log4j.Logger;
 
 public class Runner {
     private static final Logger logger = LogManager.getLogger();
-    private int x, y;
+    private int x, y, endX, endY;
     private char direction;
     private Maze maze;
+    private Path path;
 
 
-    public Runner(Maze maze) {
+    public Runner(Maze maze, Path path) {
         this.maze = maze;
+        this.path = path;
         this.x = maze.getEntryCoords()[0];
         this.y = maze.getEntryCoords()[1];
+        this.endX = maze.getExitCoords()[0];
+        this.endY = maze.getExitCoords()[1];
         this.direction = 'E';
+    }
+
+    public void setDirection(char direction) {
+        this.direction = direction;
+    }
+
+    public char getDirection() {
+        return direction;
     }
 
     public int getX() {
@@ -34,7 +46,7 @@ public class Runner {
         }
     }
 
-    private void turnRight() {
+    public void turnRight() {
         switch (direction) {
             case 'E' -> direction = 'S';
             case 'W' -> direction = 'N';
@@ -61,34 +73,46 @@ public class Runner {
         }
     }
 
-    // Validates path in canonical form 
+    // Validates path in canonical or factorized form 
     public void validatePath(String pathSequence) {
+        int moveInt = 1;
+
         OUTER:
         for (int i = 0; i < pathSequence.length(); i++) {
             char move = pathSequence.charAt(i);
-            
-            switch (move) {
-                case 'F' -> {
-                    if (!maze.canMoveForward(x, y, direction)) {
-                        logger.info("Can't move forward");
-                        break OUTER;
-                    } else {
-                        moveForward();
-                        logger.info("Moving forward: " + x + " " + y);
+
+            if (Character.isDigit(move)) {
+                moveInt = move - '0';
+                continue;
+            }
+
+            if (move == 'F' || move == 'R' || move == 'L' || move == ' ') {
+                for (int j = 0; j < moveInt; j++) {
+                    switch (move) {
+                        case 'F' -> {
+                            if (!maze.canMoveForward(x, y, direction)) {
+                                logger.info("Can't move forward");
+                                break OUTER;
+                            } else {
+                                moveForward();
+                                logger.info("Moving forward: " + x + " " + y);
+                            }
+                        }
+                        case 'R' -> {
+                            turnRight();
+                            logger.info("Turning Right: " + x + " " + y);
+                        }
+                        case 'L' -> {
+                            turnLeft();
+                            logger.info("Turning Left: " + x + " " + y);
+                        }
+                        case ' ' -> {
+                            continue;
+                        }
                     }
                 }
-                case 'R' -> {
-                    turnRight();
-                    logger.info("Turning Right: " + x + " " + y);
-                }
-                case 'L' -> {
-                    turnLeft();
-                    logger.info("Turning Left: " + x + " " + y);
-                }
-                case ' ' -> {
-                    continue;
-                }
-                default -> logger.error("Invalid Symbol");
+
+                moveInt = 1;
             }
         }
  
