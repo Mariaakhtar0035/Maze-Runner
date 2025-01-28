9a0b0b3dacd53084ae1794eec0fae25070ca86af
Mariaakhtar0035 <mariaakhtar0035@gmail.com>
2025-01-28 15:54:03 -0500

    feat: #5 output path in factorized form

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 public class Path {
     private final ArrayList<Character> sequence;
     private String pathCanonical = "";
+    private String pathFactorized = "";
 
     public Path() {
         sequence = new ArrayList<>();
@@ -14,13 +15,41 @@ public class Path {
     }
 
     public void outputPathCanonical() {
-        
+        char currentInstruction = sequence.get(0);
+
         for (char instruction : sequence) {
+            if (instruction != currentInstruction) {
+                pathCanonical += " ";
+            }
             pathCanonical += instruction;
+            currentInstruction = instruction;
         }
         
         System.out.println(pathCanonical);
 
     }
+
+    public void outputPathFactorized() {
+        char currentInstruction = sequence.get(0);
+        int count = 0;
+
+        for (char instruction : sequence) {
+            if (instruction == currentInstruction) {
+                count++;
+            }
+            else {
+                if (count != 1 ) pathFactorized += count;
+                pathFactorized = pathFactorized + currentInstruction + " ";
+                currentInstruction = instruction;
+                count = 1;
+            }
+        }
+
+        if (count != 1 ) pathFactorized += count;
+        pathFactorized = pathFactorized + currentInstruction + " ";
+        
+        System.out.println(pathFactorized);
+        
+    } 
     
 }
